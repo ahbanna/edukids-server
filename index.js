8d819e9627cbe0ea117a5755a3adba2aee5d60f9
Al Hasanul Banna <tutul5800@gmail.com>
2023-05-21 00:52:45 +0600

    my toys delete

--- index.js
+++ index.js
@@ -69,8 +69,16 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
-
     // my toys READ end
+
+    // my toys DELETE starts
+    app.delete("/mytoys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toyCollection.deleteOne(query);
+      res.send(result);
+    });
+    // my toys DELETE end
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
